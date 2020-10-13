7851346d70b9a7b84fa231eca1b6cb3367b8c8f2
amin asghari sooreh <61617353+abbas775775@users.noreply.github.com>
2020-10-13 11:25:28 +0330

    Add files via upload

---
+++ st1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Simple Iris Flower Prediction App
+This app predicts the ***Iris flower*** type!
+""")
+
+st.sidebar.header('User Input Parameters')
+st.markdown (""" [vur](https://www.youtube.com/watch?v=Eai1jaZrRDs&list=PLtqF5YXg7GLmCvTswG32NqQypOuYkPRUE&index=3)""")
